86593cff750fc4c9626d07df72d0c83881c2b068
Mohand abdalrahman <mohand.abdalrahman44@gmail.com>
2025-11-17 14:54:14 +0200

    define routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'tasks'
+  },
+  {
+    path: 'tasks',
+    loadComponent: () => import('./features/tasks-list/tasks-list.component').then(m => m.TasksListComponent)
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent)
+  },
+  { path: '**', redirectTo: 'tasks' }
+];
